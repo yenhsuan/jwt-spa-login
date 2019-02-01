00815c9c696ae4bc9a77ab7aa5efb4a13e7dfc0e
yenhsuan <yenhsuan.terry@gmail.com>
2019-02-01 01:55:47 -0800

    test route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import { hot } from 'react-hot-loader/root';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import TestPage from './components/views/TestPage';
 import './sass/main.scss';
+
+const Index = () => <h2>Home</h2>;
+const About = () => <h2>About</h2>;
+const Users = () => <h2>Users</h2>;
 
 const App = () => (
   <div className="app-root">
     test
     <div className="app-root__text">
-      dfsdsdfsdfsd
+      test111
     </div>
     <TestPage />
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about/">About</Link>
+            </li>
+            <li>
+              <Link to="/users/">Users</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Route path="/" exact component={Index} />
+        <Route path="/about/" component={About} />
+        <Route path="/users/" component={Users} />
+      </div>
+    </Router>
   </div>
 );
 
